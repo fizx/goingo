c610ee6c6d2ab7d1919de0675e9568270077a096
Kyle Maxwell <kyle@kylemaxwell.com>
2022-04-03 14:55:14 -0700

    add a desc type

--- lib.go
+++ lib.go
@@ -7,3 +7,19 @@ import (
 type RawService interface {
 	Call(ctx context.Context, name string, method string, message []byte) ([]byte, error)
 }
+
+type Desc struct {
+	Methods  []MethodDesc `json:"methods"`
+	Name     string       `json:"name"`
+	FullName string       `json:"fullName"`
+	Version  string       `json:"version"`
+}
+
+type MethodDesc struct {
+	Name           string `json:"name"`
+	FullName       string `json:"fullName"`
+	RequestType    string `json:"requestType"`
+	RequestStream  bool   `json:"requestStream"`
+	ResponseType   string `json:"responseType"`
+	ResponseStream bool   `json:"responseStream"`
+}
